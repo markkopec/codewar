be2bd73d147613e0423d888301918f884fddd28a
markkopec <marek.kopec.praca@gmail.com>
2017-02-07 12:41:55 +0100

    Create GrowthOfPopulation.java

---
+++ GrowthOfPopulation.java
@@ -0,0 +1,12 @@
+class Arge {
+    
+    public static int nbYear(int p0, double percent, int aug, int p) {
+        int i=0;
+        double c = p0;
+        
+        for(; c<p ; i++){
+          c = c*(1 + percent / 100)+aug;
+        }
+      return i;
+    }
+}
