442a2c45eb1592927a0d6b2e05a82fe70b38d6e9
markkopec <marek.kopec.praca@gmail.com>
2017-02-09 12:01:55 +0100

    Update ANeedleintheHaystack.java

--- ANeedleintheHaystack.java
+++ ANeedleintheHaystack.java
@@ -1,9 +1,9 @@
 public class Kata {
   public static String findNeedle(Object[] haystack) {
-    String doT = "needle";
-    int wynik = 0;
+
+    int result = 0;
     for(int i = 0; i < haystack.length; i++) {
-      if(haystack[i] == doT) {
+      if(haystack[i] == "needle") {
         wynik = i;
       }
     }
